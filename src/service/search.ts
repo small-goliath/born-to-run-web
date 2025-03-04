bafeaa2b8d4facad6838375a798d63d9d12f6e16
small-goliath <meang617@gmail.com>
2025-03-04 16:26:15 +0900

    api spec 변경

--- src/service/search.ts
+++ src/service/search.ts
@@ -5,21 +5,21 @@ export type SearchKeywordAll = {
 };
 
 export const searchKeywordAll = async () => {
-  const result = await (await api.get<Promise<SearchKeywordAll>>('/recent-search-keyword')).data;
+  const result = await (await api.get<Promise<SearchKeywordAll>>('/recent-search-keywords')).data;
   return result;
 };
 
 export const searchKeywordPost = async (keyword: string) => {
-  const result = await (await api.post<Promise<void>>(`/recent-search-keyword/${keyword}`, {})).data;
+  const result = await (await api.post<Promise<void>>(`/recent-search-keywords/${keyword}`, {})).data;
   return result;
 };
 
 export const searchKeywordDeleteAll = async () => {
-  const result = await (await api.delete<Promise<void>>('/recent-search-keyword')).data;
+  const result = await (await api.delete<Promise<void>>('/recent-search-keywords')).data;
   return result;
 };
 
 export const searchKeywordDelete = async (keyword: string) => {
-  const result = await (await api.delete<Promise<void>>(`/recent-search-keyword/${keyword}`)).data;
+  const result = await (await api.delete<Promise<void>>(`/recent-search-keywords/${keyword}`)).data;
   return result;
 };
